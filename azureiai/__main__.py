02d1ba965ec1001957e3a3b827d3eb7b4f8e175a
Daniel Ciborowski <dciborow@microsoft.com>
2022-05-18 15:25:41 -0400

    Create __main__.py

---
+++ azureiai/__main__.py
@@ -0,0 +1,7 @@
+#  ---------------------------------------------------------
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  ---------------------------------------------------------
+
+from azureiai import azpc_app
+
+azpc_app.main()
