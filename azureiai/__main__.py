38eeffd6f6ea092a1f7b5a4bd9cfbddcb56512d2
Daniel Ciborowski <dciborow@microsoft.com>
2022-05-19 12:34:39 -0400

    Create __main__.py (#78)
    * Create __main__.py
    
    * Update azureiai/__main__.py
    
    Co-authored-by: Brett Cannon <brett@python.org>
    
    * Autoformat with Black
    
    * Update azureiai/__main__.py
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    Co-authored-by: Brett Cannon <brett@python.org>
    Co-authored-by: Auto Black <auto-black@noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ azureiai/__main__.py
@@ -0,0 +1,9 @@
+#  ---------------------------------------------------------
+"""Execute program as module 'azureiai'"""
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  ---------------------------------------------------------
+
+from azureiai import azpc_app
+
+if __name__ == "__main__":
+    azpc_app.main()
