42d1ef6ea51c426dcac83269574b5c9c9b618a09
Daniel Ciborowski <dciborow@microsoft.com>
2022-08-01 19:13:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ SWAGGER_JSON = "Partner_Ingestion_SwaggerDocument.json"
 generate_swagger(SWAGGER_JSON)
 
 NAME = "az-partner-center-cli"
-VERSION = "0.0.27"
+VERSION = "0.0.28"
 
 REQUIRES = [
     "azure-mgmt-deploymentmanager",
