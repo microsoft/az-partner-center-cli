2b84e0cde994f6291b72f511bf7601fd906b468a
Auto Black <auto-black@noreply.github.com>
2022-08-02 20:04:28 +0000

    Autoformat with Black

--- azureiai/partner_center/offers/virtual_machine.py
+++ azureiai/partner_center/offers/virtual_machine.py
@@ -140,7 +140,11 @@ class VirtualMachine(Submission):
         return headers, json_config, url
 
     def _raise_connection_error(self, response):
-        error_string = json.dumps(response.json(), indent=4).replace("\\r", "").replace("\\n", os.linesep) if response.json() else response.status_code
+        error_string = (
+            json.dumps(response.json(), indent=4).replace("\\r", "").replace("\\n", os.linesep)
+            if response.json()
+            else response.status_code
+        )
         raise ConnectionError(error_string)
 
 
