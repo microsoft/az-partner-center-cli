aa027ed3e5f662a34c09c03e317e1966be344deb
Daniel Ciborowski <dciborow@microsoft.com>
2022-08-02 23:42:27 -0400

    Update container.py

--- azureiai/partner_center/offers/container.py
+++ azureiai/partner_center/offers/container.py
@@ -85,7 +85,7 @@ class Container(Submission):
         with open(self.config_yaml, encoding="utf8") as file:
             settings = yaml.safe_load(file)
 
-        offer_id = os.getenv("OFFER_ID", settings["id"])
+        offer_id = os.getenv("OFFER_ID", settings.get("id", "defaultId"))
         publisher_id = os.getenv("publisherId", settings["publisherId"])
 
         url = f"{URL_BASE}/{publisher_id}/offers/{offer_id}/publish?api-version=2017-10-31"
