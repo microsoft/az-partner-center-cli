bb5e2b89ff1442595de4a2b28419196d4488ce25
Daniel Ciborowski <dciborow@microsoft.com>
2022-09-14 18:49:05 -0700

    Fix OptIn to Enabled (#164)
    * Update Partner_Ingestion_SwaggerDocument.json
    
    * Update reseller_configuration.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ SWAGGER_JSON = "Partner_Ingestion_SwaggerDocument.json"
 generate_swagger(SWAGGER_JSON)
 
 NAME = "az-partner-center-cli"
-VERSION = "0.0.38"
+VERSION = "0.0.39"
 
 REQUIRES = [
     "azure-mgmt-deploymentmanager",
