0ce29a64959bffe3d3cfefd2775942a0b52437f7
Daniel Ciborowski <dciborow@microsoft.com>
2022-09-21 22:28:31 -0400

    add license to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #   ---------------------------------------------------------
 #   Copyright (c) 2020 Microsoft Corporation. All rights reserved.
+#   MIT License
 #   ---------------------------------------------------------
 """Azure Managed Applications Python SDK"""
 from os import environ
