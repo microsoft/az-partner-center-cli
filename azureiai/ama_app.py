33518a2949c654a9dfa91285973c50e2f89528d9
Daniel Ciborowski <dciborow@microsoft.com>
2022-09-24 19:28:02 -0400

    Update ama_app.py

--- azureiai/ama_app.py
+++ azureiai/ama_app.py
@@ -2,7 +2,7 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  ---------------------------------------------------------
 """
-This method has been replaced by az_app.py. These methods may not be kept up to date.
+This 'main' method has been replaced with the 'main' found in 'az_app.py'. These methods may not be kept up to date.
 They are currently kept for test coverage against APIs.
 
 CLI Wrapper for Creating, Updating, or Deleting Azure Managed Applications
@@ -18,6 +18,8 @@ from azureiai.managed_apps.commands import (
     status,
     update,
 )
+
+
 
 
 def main():
