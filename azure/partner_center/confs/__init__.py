108babfb2e7ddc2aac832f15ffd9b9f4d2faf0f0
Daniel Ciborowski <dciborow@microsoft.com>
2022-09-25 15:11:20 -0400

    fix init files

--- azure/partner_center/confs/__init__.py
+++ azure/partner_center/confs/__init__.py
@@ -2,6 +2,11 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  ---------------------------------------------------------
 """Configurations for Azure Managed Application Plans"""
+from azure.partner_center.confs.reseller_configuration import ResellerConfiguration
+from azure.partner_center.confs.listing import Listing
+from azure.partner_center.confs.listing_image import ListingImage
+from azure.partner_center.confs.product_availability import ProductAvailability
+from azure.partner_center.confs.properties import Properties
 
 __all__ = [
     "ResellerConfiguration",
--- azure/partner_center/confs/variant/__init__.py
+++ azure/partner_center/confs/variant/__init__.py
@@ -2,5 +2,8 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  ---------------------------------------------------------
 """Variant Configurations are used for setting up new plans within an offer"""
+from azure.partner_center.confs.variant.feature_availability import FeatureAvailability
+from azure.partner_center.confs.variant.offer_listing import OfferListing
+from azure.partner_center.confs.variant.package import Package
 
 __all__ = ["FeatureAvailability", "OfferListing", "Package"]
