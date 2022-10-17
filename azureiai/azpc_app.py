233db85be2268f74ac21952b4d2602ae3cd02740
Alex Koller <alexkoller@microsoft.com>
2022-10-17 11:01:35 +1100

    Fix debug bug

--- azureiai/azpc_app.py
+++ azureiai/azpc_app.py
@@ -42,7 +42,7 @@ def main():
         print(run(commands[subgroup]()), file=sys.stdout)
 
         thread_names = {t.ident: t.name for t in threading.enumerate()}
-        for thread_id, frame in sys._current_frames().iteritems():
+        for thread_id, frame in sys._current_frames().items():
             print("Thread %s:" % thread_names.get(thread_id, thread_id))
             traceback.print_stack(frame)
         print()
