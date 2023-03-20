91016e596cbc0d1fce76cd15605422f33d01eaf4
Auto Black <auto-black@noreply.github.com>
2023-03-20 19:38:04 +0000

    Auto Black Formatting Action

--- azureiai/managed_apps/confs/variant/feature_availability.py
+++ azureiai/managed_apps/confs/variant/feature_availability.py
@@ -88,7 +88,7 @@ class FeatureAvailability(VariantPlanConfiguration):
                                 "pricingModel": "Recurring",
                             }
                         ],
-                    }
+                    },
                 ],
             }
         else:
